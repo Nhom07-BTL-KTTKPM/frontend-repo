b373027227e3842360232b2eb132b1024368f12c
Nguyen Quoc Huy <122158094+Nine99Huy8181@users.noreply.github.com>
2026-05-05 15:37:54 +0700

    feat: add ui for login with google

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,24 +1,26 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import './index.css'
 import App from './App.tsx'
+
+const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: false, // Tránh call api liên tục khi focus lại tab
-      retry: 1, // Chỉ retry 1 lần mặc định cho các query lỗi
+      refetchOnWindowFocus: false,
+      retry: 1,
     },
   },
 });
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <GoogleOAuthProvider clientId={googleClientId}>
     <QueryClientProvider client={queryClient}>
       <App />
       <Toaster position="top-right" richColors closeButton />
     </QueryClientProvider>
-  </StrictMode>,
+  </GoogleOAuthProvider>,
 )
